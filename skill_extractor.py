540723be82afc439608eebc0809b6d8ac80efaf2
Emdien <gonnm8@gmail.com>
2021-02-18 15:21:13 +0100

    genPattern function implementation

--- skill_extractor.py
+++ skill_extractor.py
@@ -5,6 +5,142 @@ import regex as re
 # Objetivos: Quiero una consola que me permita extraer determinadas Skill IDs de determinadas clases y specs
 # Secundario: Extraer Skill IDs de una skill en particular indicando class, spec y nombre de skill.
 
+#Me devuelve un patron regex ajustado
+def genPattern(className, specID):
+    classString = ""
+    specString = ""
+
+    if className.lower() == "blademaster" or className.lower() == "bm":
+        classString = "BladeMaster_"
+        if specID == 1 or specID.lower() == "fire":
+            specString = "G1_"
+        elif specID == 2 or specID.lower() == "lightning":
+            specString = "G2_"
+        elif specID == 3 or specID.lower() == "spectral":
+            specString = "G3_"
+        return classString + specString
+
+    elif className.lower() == "kungfumaster" or className.lower() == "kfm":
+        classString = "KungfuMaster_"
+        if specID == 1 or specID.lower() == "fire":
+            specString = "G1_"
+        elif specID == 2 or specID.lower() == "wind":
+            specString = "G2_"
+        elif specID == 3 or specID.lower() == "wolf":
+            specString = "G3_"
+        return classString + specString
+
+    elif className.lower() == "destroyer" or className.lower() == "des":
+        classString = "Destroyer_"
+        if specID == 1 or specID.lower() == "earth":
+            specString = "G1_"
+        elif specID == 2 or specID.lower() == "shadow":
+            specString = "G2_"
+        elif specID == 3 or specID.lower() == "twin steel": #? xd Idk 
+            specString = "G3_"
+        return classString + specString
+
+    elif className.lower() == "forcemaster" or className.lower() == "fm":
+        classString = "ForceMaster_"
+        if specID == 1 or specID.lower() == "fire":
+            specString = "G1_"
+        elif specID == 2 or specID.lower() == "ice":
+            specString = "G2_"
+        elif specID == 3 or specID.lower() == "lightning":
+            specString = "G3_"
+        return classString + specString
+
+    elif className.lower() == "assasin" or className.lower() == "sin":
+        classString = "Assassin_"
+        if specID == 1 or specID.lower() == "serpent":
+            specString = "G1_"
+        elif specID == 2 or specID.lower() == "shadow":
+            specString = "G2_"
+        elif specID == 3 or specID.lower() == "phantom":
+            specString = "G3_"
+        return classString + specString
+
+    elif className.lower() == "summoner" or className.lower() == "sum":
+        classString = "Summoner_"
+        if specID == 1 or specID.lower() == "earth":
+            specString = "G1_"
+        elif specID == 2 or specID.lower() == "wind":
+            specString = "G2_"
+        elif specID == 3 or specID.lower() == "fantasy":
+            specString = "G3_"
+        return classString + specString
+
+    elif className.lower() == "warlock" or className.lower() == "wl":
+        classString = "Warlock_"
+        if specID == 1 or specID.lower() == "ice":
+            specString = "G1_"
+        elif specID == 2 or specID.lower() == "shadow":
+            specString = "G2_"
+        elif specID == 3 or specID.lower() == "tba":
+            specString = "G3_"
+        return classString + specString
+
+    elif className.lower() == "swordmaster" or className.lower() == "bd":
+        classString = "SwordMaster_"
+        if specID == 1 or specID.lower() == "wind":
+            specString = "G1_"
+        elif specID == 2 or specID.lower() == "lightning":
+            specString = "G2_"
+        elif specID == 3 or specID.lower() == "ghost":
+            specString = "G3_"
+        return classString + specString
+
+    elif className.lower() == "soulfighter" or className.lower() == "sf":
+        classString = "SoulFighter_"
+        if specID == 1 or specID.lower() == "earth":
+            specString = "G1_"
+        elif specID == 2 or specID.lower() == "ice":
+            specString = "G2_"
+        elif specID == 3 or specID.lower() == "tba":
+            specString = "G3_"
+        return classString + specString
+
+    elif className.lower() == "shooter" or className.lower() == "gun":
+        classString = "Shooter_"
+        if specID == 1 or specID.lower() == "fire":
+            specString = "G1_"
+        elif specID == 2 or specID.lower() == "shadow":
+            specString = "G2_"
+        elif specID == 3 or specID.lower() == "tba":
+            specString = "G3_"
+        return classString + specString
+
+    elif className.lower() == "warrior" or className.lower() == "war":
+        classString = "Warrior_"
+        if specID == 1 or specID.lower() == "lightning":
+            specString = "G1_"
+        elif specID == 2 or specID.lower() == "ice":
+            specString = "G2_"
+        elif specID == 3 or specID.lower() == "tba":
+            specString = "G3_"
+        return classString + specString
+
+    elif className.lower() == "archer" or className.lower() == "arc":
+        classString = "Archer_"
+        if specID == 1 or specID.lower() == "light":
+            specString = "G1_"
+        elif specID == 2 or specID.lower() == "wind":
+            specString = "G2_"
+        elif specID == 3 or specID.lower() == "tba":
+            specString = "G3_"
+        return classString + specString
+
+    elif className.lower() == "thunderer" or className.lower() == "ast":
+        classString = "Thunderer_"
+        if specID == 1 or specID.lower() == "galaxy":
+            specString = "G1_"
+        elif specID == 2 or specID.lower() == "storm":
+            specString = "G2_"
+        elif specID == 3 or specID.lower() == "tba":
+            specString = "G3_"
+        return classString + specString
+
+
 if __name__ == '__main__':
     skill_file = open(r'skills.csv', 'w', newline='')
 
